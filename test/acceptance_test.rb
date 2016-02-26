5dc417f036147ad3adb1971f7a36c7d053d73989
Vince Lundgren <vincent.lundgren@gmail.com>
2016-02-26 08:50:02 -0600

    fixed test 1 and added test 2

--- test/acceptance_test.rb
+++ test/acceptance_test.rb
@@ -1,11 +1,12 @@
-   require 'Open3'
+require 'Open3'
+
+# Add our bin directory to the PATH so that we can
+# run `notes` wiwthout specifying where to look.
+bin_dir     = File.expand_path('../../bin', __FILE__)
+ENV['PATH'] = bin_dir + ":" + ENV['PATH']
 
 class NotesTest < Minitest::Test
-
-
-
   def test_it_displays_notes_by_default_with_open_3
-
 expected = 'Add 1 to 2    1 + 2  # => 3
 Subtract 5 from 2    2 - 5  # => -3
 Is 1 less than 2    1 < 2  # => true
@@ -20,21 +21,17 @@ Access an element in an array by its index    ["a","b","c"][0] # => "a"
 Find out how big the array is    ["a","b"].length # => 2
 '
 
-notes_program = File.expand_path('../../bin/notes', __FILE__)
-stdout, stderr, exitstatus = Open3.capture3 'ruby', notes_program
-assert_equal '', stderr
-assert exitstatus.success?
-assert_equal expected, stdout
-
+    require "pry"
+    binding.pry
+    stdout, stderr, exitstatus = Open3.capture3 'notes'
+    assert_equal '', stderr
+    assert exitstatus.success?
+    assert_equal expected, stdout
   end
 
+
+  def test_it_selects_notes_whose_descriptions_matches_an_argument_from_the_command_line
+    skip
+  end
 end
-
-
-
-
-
-
-
-
 
