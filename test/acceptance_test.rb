8610d11f175b143e2e3a62f3441ff7ecb2496560
Dongmin Lee <medi8600@gmail.com>
2016-02-26 22:40:19 -0600

    branch fixing..

--- test/acceptance_test.rb
+++ test/acceptance_test.rb
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 require 'Open3'
-=======
-require 'open3'
->>>>>>> dongmin
 
 # Add our bin directory to the PATH so that we can
 # run `notes` wiwthout specifying where to look.
@@ -10,15 +6,11 @@ bin_dir     = File.expand_path('../../bin', __FILE__)
 ENV['PATH'] = bin_dir + ":" + ENV['PATH']
 #stdout, stderr, exitstatus = Open3.capture3(bin_dir)
 
-<<<<<<< HEAD
 class NotesTest < Minitest::Test
 #  def test_it_displays_notes_by_default_with_open_3
 
 
    #  def test_it_displays_notes_by_default_with_open_3
-=======
-  def test_it_displays_notes_by_default_with_open_3
->>>>>>> dongmin
 
 expected = 'Add 1 to 2    1 + 2  # => 3
 Subtract 5 from 2    2 - 5  # => -3
@@ -33,71 +25,68 @@ Remove elements in second array from first    [1,2,4] - [2, 3] # => [1,4]
 Access an element in an array by its index    ["a","b","c"][0] # => "a"
 Find out how big the array is    ["a","b"].length # => 2
 '
+
 def test_first
- # bin_dir     = File.expand_path('../../bin', __FILE__)
- # ENV['PATH'] = bin_dir + ":" + ENV['PATH']
-   stdout, stderr, exitstatus = Open3.capture3 'notes'
-   # require "pry"
-   # binding.pry
-   # stdout, stderr, exitstatus = Open3.capture3 'notes'
+  # bin_dir     = File.expand_path('../../bin', __FILE__)
+  # ENV['PATH'] = bin_dir + ":" + ENV['PATH']
+  stdout, stderr, exitstatus = Open3.capture3 'notes'
+  # require "pry"
+  # binding.pry
+  # stdout, stderr, exitstatus = Open3.capture3 'notes'
 
 
-   # assert_equal '', stderr
-   # assert exitstatus.successk?
+  # assert_equal '', stderr
+  # assert exitstatus.successk?
 
-    assert_match /Add 1 to 2/ , stdout
-   # assert_match /Convert 1 to a float    *1.to_f # => 3/, stdout
-   # assert_equal expected, stdout
-    #
+  assert_match /Add 1 to 2/ , stdout
+  # assert_match /Convert 1 to a float    *1.to_f # => 3/, stdout
+  # assert_equal expected, stdout
+  #
 
-   # require "pry"
-   # binding.pry
-  end
-<<<<<<< HEAD
-
-=======
+  # require "pry"
+  # binding.pry
 end
->>>>>>> dongmin
 
-  def test_it_selects_notes_whose_descriptions_matches_an_argument_from_the_command_line # < Vari
+
+def test_it_selects_notes_whose_descriptions_matches_an_argument_from_the_command_line # < Vari
 
   #  test = Vari.new
   # stdout, stderr, exitstatus = Open3.capture3 'notes'
-   stdout, stderr, exitstatus = Open3.capture3 'notes less'
+  stdout, stderr, exitstatus = Open3.capture3 'notes less'
   # require "pry"
   # binding.pry
   #  stdout, stderr, exitstatus = Open3.capture3 'notes'
-   assert_match /Is 1 less than 2    1 < 2  # => true/ , stdout
+  assert_match /Is 1 less than 2    1 < 2  # => true/ , stdout
 
 
 
-  end
+end
 
 
-  def test_select_notes_whose_example_matches_an_argument_from_the_command_line
+def test_select_notes_whose_example_matches_an_argument_from_the_command_line
 
-    stdout, stderr, exitstatus = Open3.capture3 'notes length'
-
-
-
-    #require "pry"
-    #binding.pry
-     #assert_match /Find out how big the array is   \[\"a\",\"b\"\].length # => 2\n/ , stdout
-     assert_match /Find out how big the array is    \[\"a\",\"b\"\].length \# \=> 2\n/ , stdout
-
-  end
+  stdout, stderr, exitstatus = Open3.capture3 'notes length'
 
 
-  def test_Matching_is_case_insensitive
-    stdout, stderr, exitstatus = Open3.capture3 'notes add'
 
-    assert_match /Add 1 to 2    1 \+ 2  \# \=> 3\n/ , stdout
+  #require "pry"
+  #binding.pry
+  #assert_match /Find out how big the array is   \[\"a\",\"b\"\].length # => 2\n/ , stdout
+  assert_match /Find out how big the array is    \[\"a\",\"b\"\].length \# \=> 2\n/ , stdout
 
-    #require "pry"
-    #binding.pry
-    stdout, stderr, exitstatus = Open3.capture3 'notes ADD'
-    assert_match /Add 1 to 2    1 \+ 2  \# \=> 3\n/ , stdout
-  end
+end
+
+
+def test_Matching_is_case_insensitive
+  stdout, stderr, exitstatus = Open3.capture3 'notes add'
+
+  assert_match /Add 1 to 2    1 \+ 2  \# \=> 3\n/ , stdout
+
+  #require "pry"
+  #binding.pry
+  stdout, stderr, exitstatus = Open3.capture3 'notes ADD'
+  assert_match /Add 1 to 2    1 \+ 2  \# \=> 3\n/ , stdout
+end
 
 
 
