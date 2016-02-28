237cd0ff96a7e834b5a754aad2cdfcbeba066d52
Vince Lundgren <vincent.lundgren@gmail.com>
2016-02-27 22:18:57 -0600

    added unit test for multiple filters and wrote code for multiple filters

--- lib/arg_scanner.rb
+++ lib/arg_scanner.rb
@@ -1,23 +1,44 @@
 class ArgScanner
 
   def arg_scanner(string, word_to_search)
-    container = []
-    if word_to_search == nil
+
+    if word_to_search == []
       return puts string
     else
+      container1 = []
+      container2 = []
       string.each do |sentence|
-        if  sentence.scan(/.*#{word_to_search}.*/i)  != []
-            container << sentence
+        if  sentence.scan(/.*#{word_to_search[0]}.*/i)  != []
+            container1 << sentence
+        end
+
+        if word_to_search[1] != nil
+          if  sentence.scan(/.*#{word_to_search[1]}.*/i)  != []
+              container2 << sentence
+          end
         end
       end
     end
-    puts container
+
+    if container1.empty? == false and container2.empty? == false
+      j = 0
+      final = []
+      while j < container1.length
+        if container1.include?(container2[j])
+          final << container2[j]
+        end
+        j+=1
+      end
+
+      return puts final
+    else
+      return puts container1
+    end
   end
 
-
-
-
 end
+
+
 # puts arg_scanner(string, word_to_search)
 
 
---
+++ test/unit_test.rb
@@ -0,0 +1,30 @@
+require 'Open3'
+
+# Add our bin directory to the PATH so that we can
+# run `notes` wiwthout specifying where to look.
+bin_dir     = File.expand_path('../../bin', __FILE__)
+ENV['PATH'] = bin_dir + ":" + ENV['PATH']
+#stdout, stderr, exitstatus = Open3.capture3(bin_dir)
+
+class NotesTest < Minitest::Test
+  #  def test_it_displays_notes_by_default_with_open_3
+
+
+  #  def test_it_displays_notes_by_default_with_open_3
+
+
+  def test_it_treats_multiple_arguments_as_successive_filters
+
+    #  test = Vari.new
+    # stdout, stderr, exitstatus = Open3.capture3 'notes'
+    stdout, stderr, exitstatus = Open3.capture3 'notes subtract from'
+    # require "pry"
+    # binding.pry
+    #  stdout, stderr, exitstatus = Open3.capture3 'notes'
+    assert_match /Subtract 5 from 2    2 - 5  # => -3/ , stdout
+
+
+
+  end
+
+end
