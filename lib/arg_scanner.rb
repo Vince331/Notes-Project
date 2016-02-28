26ce3b163cc56302d554b52e0394beb58debf352
Vince Lundgren <vincent.lundgren@gmail.com>
2016-02-27 19:48:15 -0600

    changed structure of notes and chenges regex to search for multiple occurances and all tests pass so far

--- lib/arg_scanner.rb
+++ lib/arg_scanner.rb
@@ -1,15 +1,24 @@
 class ArgScanner
 
-def arg_scanner(string, word_to_search)
-  if word_to_search == nil
-  return puts string
-  else
-  match = string[/.*#{word_to_search}.*/i]
-  return puts match
+  def arg_scanner(string, word_to_search)
+    container = []
+    if word_to_search == nil
+      return puts string
+    else
+      string.each do |sentence|
+        if  sentence.scan(/.*#{word_to_search}.*/i)  != []
+            container << sentence
+        end
+      end
+    end
+    puts container
   end
+
+
+
+
 end
 # puts arg_scanner(string, word_to_search)
 
-end
 
 
